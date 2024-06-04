d449691f6f6aeee785dd62690fe2512ec44c1954
Trinidad, Kian Lee B. <trinidadkianleeb@gmail.com>
2024-06-04 23:00:53 +0800

    fix error

--- trinidad_kianlee/script.js
+++ trinidad_kianlee/script.js
@@ -40,7 +40,9 @@ function displayComments() {
         <span class="comment-date">${new Date(comment.date).toLocaleString()}
       </span>
       </div>
-      <p class="comment-text">${comment.text}</p></div>`).join("");
+      <p class="comment-text">${comment.text}</p></div>`
+    )
+    .join("");
 }
 
 function sortComments(order) {
@@ -57,14 +59,18 @@ function handleSortChange() {
   sortComments(sortOrder);
 }
 
-document.getElementById("search_button").addEventListener("click", function(){
-  var country = document.getElementById("country_input").value;
+document.getElementById("search_button").addEventListener("click", function (){
+  let country = document.getElementById("country_input").value;
   fetch("https://restcountries.com/v3.1/name/" + country)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Country not found");
+      }
+      return response.json();
+    })
     .then((data) => {
       let countryDetails = document.getElementById("country_details");
-      countryDetails.innerHTML = `
-          <h2>${data[0].name.common}</h2>
+      countryDetails.innerHTML = `<h2>${data[0].name.common}</h2>
           <img src="${data[0].flags.png}" alt="${data[0].name.common} flag">
           <p>Capital: ${data[0].capital[0]}</p>
           <p>Region: ${data[0].region}</p>
@@ -73,16 +79,18 @@ document.getElementById("search_button").addEventListener("click", function(){
           <p>Area: ${data[0].area} sq km</p>`;
       return data[0].region;
     })
-    .then((region) => {
-      fetch("https://restcountries.com/v3.1/region/" + region)
-        .then((response) => response.json())
-        .then((data) => {
-        let regionCountries = document.getElementById("region_countries");
-        regionCountries.innerHTML = "<h2>Countries in the same region:</h2>";
-        data.forEach((country) => {regionCountries.innerHTML += 
-          `<p>${country.name.common}</p>
-          <img src="${country.flags.png}" alt="${country.name.common} flag">`;
-          });
-        });
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data || !Array.isArray(data) || data.length === 0) {
+        console.error("Invalid data");
+        return;
+      }
+
+      let regionCountries = document.getElementById("region_countries");
+      regionCountries.innerHTML = `<h2>Countries in the same region:</h2>`;
+      data.forEach((country) => {
+      regionCountries.innerHTML += `<p>${country.name.common}</p>
+      <img src="${country.flags.png}" alt="${country.name.common} flag">`;
+      });
     });
 });
